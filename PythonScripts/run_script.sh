8c2b6b31f7a1888ccf7c76e60b7249d4494c74c6
Rodrigo Lucas Pinto da Silva <rodrigo.lpdsilva@gmail.com>
2020-02-05 11:50:25 -0300

    Outputs de projecao e samples resumidos para minimizar consumo de armazenamento

--- PythonScripts/run_script.sh
+++ PythonScripts/run_script.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 source /home/cendas/miniconda3/bin/activate DataEnv
 
+CHANNELS=('CH01' 'CH02' 'CH03' 'CH04' 'CH05' 'CH06' 'CH07' 'CH08' 'CH09' 'CH10' 'CH11' 'CH12' 'CH13' 'CH14' 'CH15' 'CH16')
+qtdCH=${#CHANNELS[@]}
+
 /home/cendas/miniconda3/envs/DataEnv/bin/python3 /home/cendas/GOES16-Files/CodeProcess/PythonScripts/monitorRJ.py
 printf "\nAs projecoes centradas no Rio de Janeiro estao atualizadas\n\n"
 
-export WORK='/home/cendas/GOES16-Files/Output/RJ/Projections/CH13/'
-
+#Deixa apenas 10 projecoes/imagens no diretorio
+for ((Channel=0; Channel< qtdCH; Channel++)) do
+export WORK='/home/cendas/GOES16-Files/Output/RJ/Projections/'${CHANNELS[$Channel]}'/'
 cd $WORK
 
-ls -t | tail -n +11 | xargs rm --
+ls -t | tail -n +11 | xargs rm 
+
+done
+
 
 /home/cendas/miniconda3/envs/DataEnv/bin/python3 /home/cendas/GOES16-Files/CodeProcess/PythonScripts/monitorSouthAmerica.py
 
 printf "\nAs projecoes Sulamericanas estao atualizadas\n\n" 
 
-export WORK='/home/cendas/GOES16-Files/Output/South_America/Projections/CH13/'
+#Deixa apenas 10 projecoes/imagens no diretorio
+for ((Channel=0; Channel< qtdCH; Channel++)) do
+export WORK='/home/cendas/GOES16-Files/Output/South_America/Projections/'${CHANNELS[$Channel]}'/'
 
 cd $WORK
 
 ls -t | tail -n +11 | xargs rm --
+
+done
+
+#Deixa apenas os 160 arquivos NetCDF no diretorio
+export WORK='/home/cendas/GOES16-Files/Samples/'
+cd $WORK
+
+ls -t | tail -n +161 | xargs rm 
