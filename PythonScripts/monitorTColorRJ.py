1fa0bcfdb2f7e1bf882ec07430c15d009d9512ef
Rodrigo Lucas Pinto da Silva <rodrigo.lpdsilva@gmail.com>
2020-03-20 14:48:42 -0300

    Pace implementado

--- PythonScripts/monitorTColorRJ.py
+++ PythonScripts/monitorTColorRJ.py
@@ -28,7 +28,7 @@ for posi in range(len(singular)):
       #print(G16_images[count])
       pass
   #print()  
-#print(G16_images)
+#print(G16_images)sss
 
 # If the log file doesn't exist yet, create one
 file = open('/home/cendas/GOES16-Files/CodeProcess/PythonScripts/TrueColor.txt', 'a')
@@ -45,7 +45,44 @@ with open('/home/cendas/GOES16-Files/CodeProcess/PythonScripts/TrueColor.txt') a
 logTrueColor = [x.strip() for x in logTrueColor]
 #print(logTrueColor)
 
+ch01 = ""
+ch02 = ""
+ch03 = ""
 
+validateCH = []
+pace = int((len(G16_images))/3)
+G16_images =  sorted(G16_images)
+for x in range (int(len(G16_images)/3)):
+  validateCH01 = False
+  validateCH02 = False
+  validateCH03 = False
+  
+  ch01 = G16_images[x]  
+  ch02 = G16_images[x + pace]
+  ch03 = G16_images[x + 2*pace]
+  
+  print(logTrueColor.count (ch01))
+  print(logTrueColor.count (ch02))
+  print(logTrueColor.count (ch03))
+  
+  if logTrueColor.count (ch01) == 0:
+    validateCH01 = True
+  if logTrueColor.count (ch02) == 0:
+    validateCH02 = True
+  if logTrueColor.count (ch03) == 0:
+    validateCH03 = True
+  if (validateCH01 and validateCH02 and validateCH03):
+    validateCH.append (ch01)
+    validateCH.append (ch02)
+    validateCH.append (ch03)
+
+#print (G16_images)
+print()
+#print (logTrueColor)  
+#print()    
+print (validateCH)   
+    
+  
 # Compare the directory list with the file list
 # Loop through all files in the directory
 
@@ -55,6 +92,6 @@ logTrueColor = [x.strip() for x in logTrueColor]
 #  os.system("/home/cendas/miniconda3/envs/DataEnv/bin/python3 " + "\"/home/cendas/GOES16-Files/CodeProcess/PythonScripts/process_goes-16_RJ_Projection.py\"" + " " + "\"" + x.replace('\\','\\\\') + "\"")
   #os.system("/home/cendas/miniconda3/envs/DataEnv/bin/python3 " + "\"/home/cendas/GOES16-Files/CodeProcess/PythonScripts/TemperatureDataToGeojsonRJ.py\"" + " " + "\"" + x.replace('\\','\\\\') + "\"")
   
-for x in range (int(len(logTrueColor)/3)): #De 3 em 3
+#for x in range (int(len(logTrueColor)/3)): #De 3 em 3
   
-  os.system("/home/cendas/miniconda3/envs/DataEnv/bin/python3 " + "\"/home/cendas/GOES16-Files/CodeProcess/PythonScripts/trueColorRJ.py\"" + " " + logTrueColor[x].replace('\\','\\\\') + " "+ logTrueColor[x+1].replace('\\','\\\\')+ " " + logTrueColor[x+2].replace('\\','\\\\'))
+#  os.system("/home/cendas/miniconda3/envs/DataEnv/bin/python3 " + "\"/home/cendas/GOES16-Files/CodeProcess/PythonScripts/trueColorRJ.py\"" + " " + logTrueColor[x].replace('\\','\\\\') + " "+ logTrueColor[x+1].replace('\\','\\\\')+ " " + logTrueColor[x+2].replace('\\','\\\\'))
