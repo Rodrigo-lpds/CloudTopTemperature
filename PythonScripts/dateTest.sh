1fdf1d551f64e493ea0b92f54b371d76dc7dd71a
Rodrigo <rodrigo.lpdsilva@gmail.com>
2020-03-24 22:21:08 -0300

    Pasta Python Script removida

--- PythonScripts/dateTest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export WORK='/home/cendas/GOES16-Files/Output/RJ/Projections/CH13/'
-
-echo $WORK
-
-cd $WORK
-
-ls -t | tail -n +11 | xargs rm --
--- PythonScripts/sendProj.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-export WORK="/home/cendas/GOES16-Files/Output/RJ/SistConvectivos/"  
-cd $WORK
-#Pega ultimo jpg processado na lista
-ultimo=`tail -n1 Lista_JPG.txt`
-#copia para a outra maquina via SSH
-scp -P7654 ${ultimo} faho@146.164.165.132:/u/home/cendas/plataforma/satelite/SistConvectivos_RJ/.
-
